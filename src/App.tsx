2d8bbd0fa7c22cddecefeb899836c31d1aff6eea
Caio Giffoni <caio_cgf@hotmail.com>
2022-11-20 16:42:26 -0300

    :sparkles: layout desktop completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box, Icon, Text } from "@chakra-ui/react";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { BiRefresh } from "react-icons/bi";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box
+      w="100vw"
+      h="100vh"
+      bgColor="#1E555C"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        w="500px"
+        minH="440"
+        color="white"
+        fontFamily="Times new roman"
+        display="flex"
+        flexDir="column"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Text fontSize="50px">25 + 5 Clock</Text>
+        {/* Set Time Components */}
+        <Box display="flex" mt="15px" gap="50px">
+          <Box fontSize="30px">
+            <Text>Break Length</Text>
+            <Box
+              display="flex"
+              gap="20px"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Icon as={AiOutlineArrowDown} />
+              <Text>5</Text>
+              <Icon as={AiOutlineArrowUp} />
+            </Box>
+          </Box>
+          <Box fontSize="30px">
+            <Text>Session Length</Text>
+            <Box
+              display="flex"
+              gap="20px"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Icon as={AiOutlineArrowDown} />
+              <Text>25</Text>
+              <Icon as={AiOutlineArrowUp} />
+            </Box>
+          </Box>
+        </Box>
+        {/* Clock */}
+        <Box
+          w="274px"
+          h="194px"
+          border="7px solid #13353a"
+          borderRadius="50px"
+          mt="20px"
+          display="flex"
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize="30px">Session</Text>
+          <Text fontSize="80px">25:00</Text>
+        </Box>
+        {/* Controls */}
+        <Box mt="20px" display="flex" gap="12px">
+          <Icon w="40px" h="40px" as={BsFillPlayFill} />
+          <Icon w="40px" h="40px" as={BsFillPauseFill} />
+          <Icon w="40px" h="40px" as={BiRefresh} />
+        </Box>
+        <Text>Coded by</Text>
+        <Text color="teal.200">Caio Giffoni</Text>
+      </Box>
+    </Box>
+  );
 }
 
-export default App
+export default App;
